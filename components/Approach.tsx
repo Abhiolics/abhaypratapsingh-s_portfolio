77fc6fe2b6ce1581c72c6e63c7ecb69d54e5f8d6
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2024-06-10 13:43:43 +0530

    bug fixed

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -1,70 +1,69 @@
 "use client";
 import React from "react";
-
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "./ui/CanvasRevealEffect";
 
-const Approach = () =>  {
+const Approach = () => {
   return (
     <section className="w-full py-20">
-    <h1 className="heading">
-      My <span className="text-purple">approach</span>
-    </h1>
-    {/* remove bg-white dark:bg-black */}
-    <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
-      {/* add des prop */}
-      <Card
-        title="Planning & Strategy"
-        icon={<AceternityIcon order="Phase 1" />}
-        description="We'll collaborate to map out your website's goals, target audience, 
-        and key functionalities. We'll discuss things like site structure, 
-        navigation, and content requirements."
-      >
-        <CanvasRevealEffect
-          animationSpeed={5.1}
-          // add these classed for the border rounded overflowing -> rounded-3xl overflow-hidden
-          containerClassName="bg-emerald-900 rounded-3xl overflow-hidden"
-        />
-      </Card>
-      <Card
-        title="Development & Progress Update"
-        icon={<AceternityIcon order="Phase 2" />}
-        description="Once we agree on the plan, I cue my lofi playlist and dive into
-        coding. From initial sketches to polished code, I keep you updated
-        every step of the way."
-      >
-        <CanvasRevealEffect
-          animationSpeed={3}
-          // change bg-black to bg-pink-900
-          containerClassName="bg-pink-900 rounded-3xl overflow-hidden"
-          colors={[
-            // change the colors of the
-            [255, 166, 158],
-            [221, 255, 247],
-          ]}
-          dotSize={2}
-        />
-        {/* Radial gradient for the cute fade */}
-        {/* remove this one */}
-        {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
-      </Card>
-      <Card
-        title="Development & Launch"
-        icon={<AceternityIcon order="Phase 3" />}
-        description="This is where the magic happens! Based on the approved design, 
-        I'll translate everything into functional code, building your website
-        from the ground up."
-      >
-        <CanvasRevealEffect
-          animationSpeed={3}
-          containerClassName="bg-sky-600 rounded-3xl overflow-hidden"
-          colors={[[125, 211, 252]]}
-        />
-      </Card>
-    </div>
-  </section>
+      <h1 className="heading">
+        My <span className="text-purple">approach</span>
+      </h1>
+      {/* remove bg-white dark:bg-black */}
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
+        {/* add description prop */}
+        <Card
+          title="Planning & Strategy"
+          icon={<AceternityIcon order="Phase 1" />}
+          description="We'll collaborate to map out your website's goals, target audience, 
+          and key functionalities. We'll discuss things like site structure, 
+          navigation, and content requirements."
+        >
+          <CanvasRevealEffect
+            animationSpeed={5.1}
+            // add these classes for the border rounded overflowing -> rounded-3xl overflow-hidden
+            containerClassName="bg-emerald-900 rounded-3xl overflow-hidden"
+          />
+        </Card>
+        <Card
+          title="Development & Progress Update"
+          icon={<AceternityIcon order="Phase 2" />}
+          description="Once we agree on the plan, I cue my lofi playlist and dive into
+          coding. From initial sketches to polished code, I keep you updated
+          every step of the way."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            // change bg-black to bg-pink-900
+            containerClassName="bg-pink-900 rounded-3xl overflow-hidden"
+            colors={[
+              // change the colors of the
+              [255, 166, 158],
+              [221, 255, 247],
+            ]}
+            dotSize={2}
+          />
+          {/* Radial gradient for the cute fade */}
+          {/* remove this one */}
+          {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
+        </Card>
+        <Card
+          title="Development & Launch"
+          icon={<AceternityIcon order="Phase 3" />}
+          description="This is where the magic happens! Based on the approved design, 
+          I'll translate everything into functional code, building your website
+          from the ground up."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-sky-600 rounded-3xl overflow-hidden"
+            colors={[[125, 211, 252]]}
+          />
+        </Card>
+      </div>
+    </section>
   );
-}
+};
 
 const Card = ({
   title,
@@ -75,14 +74,14 @@ const Card = ({
   title: string;
   icon: React.ReactNode;
   children?: React.ReactNode;
-  discription: string;
+  description: string;
 }) => {
   const [hovered, setHovered] = React.useState(false);
   return (
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full relative mx-auto p-4  h-[35rem] "
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full relative mx-auto p-4 h-[35rem] "
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -102,13 +101,13 @@ const Card = ({
       </AnimatePresence>
 
       <div className="relative z-20">
-        <div className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
+        <div className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200 w-full mx-auto flex items-center justify-center">
           {icon}
         </div>
-        <h2 className="dark:text-white text-3xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 text-center transition duration-200">
+        <h2 className="dark:text-white text-3xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 text-center transition duration-200">
           {title}
         </h2>
-        <h2 className="dark:text-white text-center  text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200" style={{color: '#e4ecff'}}>
+        <h2 className="dark:text-white text-center text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200" style={{ color: '#e4ecff' }}>
           {description}
         </h2>
       </div>
@@ -116,10 +115,10 @@ const Card = ({
   );
 };
 
-const AceternityIcon = ({ order} : {order : string}) => {
+const AceternityIcon = ({ order }: { order: string }) => {
   return (
     <div>
-         <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         {order}
       </button>
     </div>
@@ -141,7 +140,5 @@ export const Icon = ({ className, ...rest }: any) => {
     </svg>
   );
 };
-
-
 
 export default Approach;
